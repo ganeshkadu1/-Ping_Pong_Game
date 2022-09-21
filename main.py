4f74930cfe377c6900b73f6c0f0e214439769ef3
Ganesh Kadu <64672771+ganeshkadu1@users.noreply.github.com>
2022-09-21 21:09:37 +0530

    Updated a Code

---
+++ main.py
@@ -0,0 +1,50 @@
+import time
+from turtle import Screen
+from paddle import Paddle, X_COR
+from ball import Ball
+from scoreboard import Scoreboard
+
+screen = Screen()
+l_paddle = Paddle(-X_COR, 0)
+r_paddle = Paddle(X_COR, 0)
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Ping Pong Game")
+screen.tracer(0)
+screen.listen()
+
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect Collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect Collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect l paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+X_COR = 350
+
+
+class Paddle(Turtle):
+    def __init__(self, x_cor, y_cor):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(x_cor, y_cor)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
